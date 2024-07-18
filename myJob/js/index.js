9944094cee0f976da9981d9f5a703e6cd0be7ae9
vinicius <viniciuseloi10@gmail.com>
2024-07-18 17:06:31 -0300

    buchas done

--- myJob/js/index.js
+++ myJob/js/index.js
@@ -39,6 +39,9 @@ document.addEventListener('DOMContentLoaded', () =>{
     // fucao filtrando pesquisa dos batedores
     filtroBatedor()
 
+    // funcao filtrando pesquisa das buchas
+    filtroBuchas()
+
 
     // funcoes do fast-search para mostrar pagina
     mostrarInfoFastSearch()
@@ -1219,6 +1222,7 @@ function filtroBarras(){
                 allBtns[4].classList.add('active')
             }
         }
+
     })
 }
 
@@ -1300,7 +1304,104 @@ function filtroBatedor(){
     })
 }
 
+// fucao filtrando pesquisa das buchas
+function filtroBuchas(){
 
+    let allBtns = document.querySelectorAll("#row-buchas #btns-buchas button")
+    //console.log(allBtns)
+
+    let allCards = document.querySelectorAll('#row-buchas .card')
+    //console.log(allCards)
+
+    // BTN TODOS
+    allBtns[0].addEventListener('click', () =>{
+        for(let c = 0;c < allCards.length;c++){
+            allCards[c].style.display='block'
+        }
+
+        for(let b = 0;b < allBtns.length;b++){
+            if(allBtns[0]){
+                allBtns[b].classList.remove('active')
+                allBtns[0].classList.add('active')
+            }
+        }
+    })
+
+    // BTN fiat
+    allBtns[1].addEventListener('click', () =>{
+        for(let c = 0;c < allCards.length;c++){
+            allCards[c].style.display='block'
+
+            if(!(allCards[c].classList.contains('fiat'))){
+                allCards[c].style.display='none'
+                
+            }
+        } 
+
+        for(let b = 0;b < allBtns.length;b++){
+            if(allBtns[1]){
+                allBtns[b].classList.remove('active')
+                allBtns[1].classList.add('active')
+            }
+        }
+    })
+
+    // BTN vws
+    allBtns[2].addEventListener('click', () =>{
+        for(let c = 0;c < allCards.length;c++){
+            allCards[c].style.display='block'
+
+            if(!(allCards[c].classList.contains('vws'))){
+                allCards[c].style.display='none'
+            }
+        } 
+
+        for(let b = 0;b < allBtns.length;b++){
+            if(allBtns[2]){
+                allBtns[b].classList.remove('active')
+                allBtns[2].classList.add('active')
+            }
+        }
+    })
+
+    // BTN GM
+    allBtns[3].addEventListener('click', () =>{
+        for(let c = 0;c < allCards.length;c++){
+            allCards[c].style.display='block'
+
+            if(!(allCards[c].classList.contains('gm'))){
+                allCards[c].style.display='none'
+            }
+        } 
+
+        for(let b = 0;b < allBtns.length;b++){
+            if(allBtns[3]){
+                allBtns[b].classList.remove('active')
+                allBtns[3].classList.add('active')
+            }
+        }
+    })
+
+    // BTN Ford
+    allBtns[4].addEventListener('click', () =>{
+        for(let c = 0;c < allCards.length;c++){
+            allCards[c].style.display='block'
+
+            if(!(allCards[c].classList.contains('ford'))){
+                allCards[c].style.display='none'
+            }
+        } 
+
+        for(let b = 0;b < allBtns.length;b++){
+            if(allBtns[4]){
+                allBtns[b].classList.remove('active')
+                allBtns[4].classList.add('active')
+            }
+        }
+        
+    })
+
+}
 
 
 // logica offcanvas 
